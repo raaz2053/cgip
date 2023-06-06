e13900c9b7fc60e3a1f6f0d7e31bdd1cc0087c6e
Raju Sharan <raaz2053@gmail.com>
2023-06-06 20:47:30 +0545

    Update Lab4c.m

--- Lab4c.m
+++ Lab4c.m
@@ -1,9 +1,8 @@
-pkg load image
-
-f=imread('coffee.jpg');
+clc;    % Clear the command window.
+close all;  % Close all figures (except those of imtool.)
+clear;  % Erase all existing variables. Or clearvars if you want.
+f=imread('cameraman.tif');
 f=im2double(f);
-h=rgb2gray(imread('coffee.jpg'));
-hist = histeq(h);
 w=input('Enter the Neighborhood or Window size : ');
 k=input('Enter the value of the constant k (value should be between 0 and 1) : ');
 M=mean2(f);
@@ -11,9 +10,5 @@ z=colfilt(f,[w w],'sliding',@std);
 m=colfilt(f,[w w],'sliding',@mean);
 A=k*M./z;
 g=A.*(f-m)+m;
-subplot(321), imshow(f)
-subplot(322), imhist(f)
-subplot(323), imshow(g)
-subplot(324), imhist(g)
-subplot(325), imshow(f)
-subplot(326), imhist(hist)
+subplot(121),imshow(f);
+subplot(122), imshow(g);
